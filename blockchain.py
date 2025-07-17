a989f8ad640acc1e743d8a855d7be3d1ec64e499
CatInAWizardHat <125680751+CatInAWizardHat@users.noreply.github.com>
2025-07-17 08:45:15 -0700

    Fixed syntax error in new_transaction()

--- blockchain.py
+++ blockchain.py
@@ -114,9 +114,9 @@ blockchain = Blockchain()
 
 @app.route("/mine", methods=["GET"])
 def mine():
-    # Run proof of work algorithm to get the next proof 
+    # Run proof of work algorithm to get the next proof
     last_block = blockchain.last_block
-    last_proof = last_block['proof']
+    last_proof = last_block["proof"]
     proof = blockchain.proof_of_work(last_proof)
 
     # We must receive a reward for finding the proof.
@@ -132,28 +132,32 @@ def mine():
     block = blockchain.new_block(proof, previous_hash)
 
     response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions':block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
+        "message": "New Block Forged",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": block["proof"],
+        "previous_hash": block["previous_hash"],
     }
     return jsonify(response), 200
+
 
 @app.route("/transactions/new", methods=["POST"])
 def new_transaction():
     values = request.get_json()
 
     # Check that the required fields are in POST payload
-    required = ['sender', 'recipient', 'amount']
-    if not all(k values for k in required):
-        return 'Missing values', 400
+    required = ["sender", "recipient", "amount"]
+    if not all(k in values for k in required):
+        return "Missing values", 400
 
     # Create a new transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    index = blockchain.new_transaction(
+        values["sender"], values["recipient"], values["amount"]
+    )
 
-    response = {'message': f'Transaction will be added to Block {index}'}
+    response = {"message": f"Transaction will be added to Block {index}"}
     return jsonify(response), 201
+
 
 @app.route("chain", methods=["GET"])
 def full_chain():
