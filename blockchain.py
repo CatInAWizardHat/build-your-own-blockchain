bc7daf4155fe709215274bd06094c507a1f9f9c4
CatInAWizardHat <125680751+CatInAWizardHat@users.noreply.github.com>
2025-07-16 21:53:58 -0700

    Defined new_transaction()

--- blockchain.py
+++ blockchain.py
@@ -119,8 +119,18 @@ def mine():
 
 @app.route("/transactions/new", methods=["POST"])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
 
+    # Check that the required fields are in POST payload
+    required = ['sender', 'recipient', 'amount']
+    if not all(k values for k in required):
+        return 'Missing values', 400
+
+    # Create a new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route("chain", methods=["GET"])
 def full_chain():
